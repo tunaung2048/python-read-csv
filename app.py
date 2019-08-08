edc12f65e5145ea42e4137529fa0fe5783e37b1d
Tun Tun Aung <tunaung2048@gmail.com>
2019-08-09 00:23:15 +0630

    Initial Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+data1 = pd.read_csv('list1.csv', sep=',', header=None).values
+data2 = pd.read_csv('list2.csv', sep=',', header=None).values
+
+list1 = []
+list2 = []
+# result = [x for x in data ]
+for a in data1:
+    for x in a:
+        # print (x)
+        list1.append(x)
+
+
+for a in data2:
+    for x in a:
+        # print (x)
+        list2.append(x)
+
+# print(list1)
+# print(list2)
+
+result = [x for x in list1 if x not in list2]
+
+print(result)
